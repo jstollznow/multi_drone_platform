569052130d797fa01055b995687fa2c373a73948
jstollznow <jstollznow@hotmail.com>
2019-09-11 10:13:23 +0930

    virtualflie launch

--- src/objects/rigidBody.h
+++ src/objects/rigidBody.h
@@ -2,7 +2,15 @@
 class rigidBody
 {
     private:
+        // platformID
         int platID;
+        //rigid body tag
         std::string rigidBody;
     
+
+    // std::vector<rigidBody*> arr;
+    // rigidBody* body = arr[0];
+    // if (body->isDrone()) {
+    //     drone* d = (drone*)body;
+    // }
 };
