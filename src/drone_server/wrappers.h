85529fe22067b4774f5a29050ebc9ef8efc0e445
jstollznow <jstollznow@hotmail.com>
2019-10-09 21:48:21 +1030

    yessirrrrr

--- src/drone_server/wrappers.h
+++ src/drone_server/wrappers.h
@@ -3,14 +3,14 @@
 #include <map>
 #include "../objects/rigidBody.h"
 
-#include "../wrappers/vflie.cpp"
 #include "../wrappers/cflie.cpp"
 #include "../wrappers/tello.cpp"
+#include "../wrappers/vflie.cpp"
 
 std::map<const char*, unsigned int> drone_type_map = {
-    {"vflie", 0},
-    {"cflie", 1},
-    {"tello", 2}
+    {"cflie", 0},
+    {"tello", 1},
+    {"vflie", 2}
 };
 
 namespace mdp_wrappers{
@@ -18,9 +18,9 @@ rigidBody* createNewRigidbody(std::string pTag)
 {
     std::string DroneType = pTag.substr(0, pTag.find_last_of('_'));
     switch(drone_type_map[DroneType.c_str()]) {
-        case 0: {return (rigidBody*)(new vflie(pTag));}
-        case 1: {return (rigidBody*)(new cflie(pTag));}
-        case 2: {return (rigidBody*)(new tello(pTag));}
+        case 0: {return (rigidBody*)(new cflie(pTag));}
+        case 1: {return (rigidBody*)(new tello(pTag));}
+        case 2: {return (rigidBody*)(new vflie(pTag));}
         default: return nullptr;
     }
 }
