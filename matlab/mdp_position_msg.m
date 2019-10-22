a265b4fcbda10e7ddedc7aadbbf31cff993572d2
awoolfall <8043586+awoolfall@users.noreply.github.com>
2019-10-22 18:10:29 +1030

    matlab api update

---
+++ matlab/mdp_position_msg.m
@@ -0,0 +1,10 @@
+
+classdef mdp_position_msg
+    properties
+        Position = zeros(1, 3)
+        Relative = false
+        KeepHeight = false
+        Duration = 2.0
+        Yaw = 0.0
+    end
+end
---
+++ matlab/mdp_velocity_msg.m
@@ -0,0 +1,10 @@
+
+classdef mdp_velocity_msg
+    properties
+        Velocity = zeros(1, 3)
+        Relative = false
+        KeepHeight = false
+        Duration = 2.0
+        YawRate = 0.0
+    end
+end
