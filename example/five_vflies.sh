fa19b21cc3633ae1417f087eb1e736870d7b3363
jstollznow <jstollznow@hotmail.com>
2020-05-19 00:00:52 +0930

    five_vflies script

---
+++ example/five_vflies.sh
@@ -0,0 +1,33 @@
+rosrun multi_drone_platform add_drone <<!
+
+NEW
+vflie_00
+0.0
+1.0
+
+NEW
+vflie_01
+1.0
+2.0
+
+NEW
+vflie_02
+-1.0
+-1.0
+
+NEW
+vflie_03
+2.0
+2.0
+
+NEW
+vflie_04
+-1.0
+-2.0
+
+END
+!
+
+rosrun multi_drone_platform all_debug_windows&
+
+~/MATLAB/bin/matlab -nodesktop -nosplash -r "addpath('~/catkin_ws/src/multi_drone_platform/matlab'); run('~/catkin_ws/src/multi_drone_platform/matlab/scripts/mdp_gen_graphing.m');quit();"
